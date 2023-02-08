83610496809aec812bd0510f8ff001f039f4a623
SteveZaDev <118739097+SteveZaDev@users.noreply.github.com>
2023-02-08 14:49:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,77 @@
 // pick up video at 23:40
-const wordsArray = [
-  ["GEORGE WASHINGTON", "JOHN ADAMS", "THOMAS JEFFERSON", "JAMES MADISON", "JAMES MONROE"],
-  ["ALABAMA", "ALASKA", "ARKANSAS", "CALIFORNIA"], 
-  ["MONTGOMERY", "JUNEAU", "LITTLE ROCK", "SACRAMENTO"],
-  ["CLARK GABLE", "MYRNA LOY", "CHARLES LAUGHTON"], 
-  ["WINGS", "CAVALCADE", "YOU CANT TAKE IT WIT"]
+const protoWordsArray = [
+  { 
+    cat: "Presidents",
+    sel: true,
+    items: ["GEORGE WASHINGTON", "JOHN ADAMS", "THOMAS JEFFERSON", "JAMES MADISON", "JAMES MONROE",
+  "JOHN QUINCY ADAMS", "ANDREW JACKSON"]
+  },
+  { 
+    cat: "States",
+    sel: true,
+    items: ["ALABAMA", "ALASKA", "ARKANSAS", "CALIFORNIA"]
+  },
+  { 
+    cat: "Cities",
+    sel: true,
+    items: ["MONTGOMERY", "JUNEAU", "LITTLE ROCK", "SACRAMENTO"]
+    },
+  { 
+    cat: "Countries",
+    sel: true,
+    items: ["ALBANIA", "ANDORRA"]
+  },
+  { 
+    cat: "World Cities",
+    sel: true,
+    items: ["LONDON", "PARIS"]
+    },
+   { 
+  cat: "Actors",
+  sel: true,
+  items: ["CLARK GABLE", "MYRNA LOY", "CHARLES LAUGHTON"]
+  },
+   { 
+  cat: "Movies",
+  sel: true,
+  items: ["WINGS", "CAVALCADE", "CIMMARON", "GRAND HOTEL"]
+  }
+  /*,
+  , 
+  ,
+  , 
+  */
 ]
 
+
+wordsArray=[]
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  //initHelpModal();
+  //initStatsModal();
+
   let messageContainerEl = document.getElementById('message-container')
+
   let numofLetters = 5
   let numofGuesses = 6
+  for (i=0; i<protoWordsArray.length; i++){
+    if (protoWordsArray[i].sel){
+       wordsArray.push(protoWordsArray[i])
+    }
+  }
   
   let randomArray = wordsArray[Math.floor(Math.random()*wordsArray.length)];
   randomArray = Math.floor(Math.random()*wordsArray.length);
-  randomWordle = Math.floor(Math.random()*wordsArray[randomArray].length);
+  randomWordle = Math.floor(Math.random()*(wordsArray[randomArray].items).length);
   console.log("randomArray = " + randomArray) 
-  console.log(wordsArray[randomArray] + "length of array = " + wordsArray[randomArray].length)
-  numofLetters = wordsArray[randomArray][randomWordle].length;
-  console.log(wordsArray[randomArray][randomWordle] + " has " + numofLetters + " letters in it")
-  let wordle = wordsArray[randomArray][randomWordle];
+  console.log("randomWordle = " + randomWordle) 
+ // console.log(wordsArray[randomArray] + "length of array = " + wordsArray[randomArray].length)
+  numofLetters = (wordsArray[randomArray].items[randomWordle]).length;
+//  console.log(wordsArray[randomArray][randomWordle] + " has " + numofLetters + " letters in it")
+  let wordle = wordsArray[randomArray].items[randomWordle];
+  console.log("wordle = " + wordle)
   let guessedWordCount = 0;
   numofGuesses = 6;
   if (numofLetters > 10){
@@ -27,87 +79,146 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   createSquares();
+  addKeyboardClicks();
 
   let guessedWords = [[]]
   let availableSpace = 1; 
 
   const keys = document.querySelectorAll('.keyboard-row button')
 
-
+  document.addEventListener('keydown', function(event) {
+    console.log ("key pressed was " + event.key)
+    handleKeystroke(event.key)
+    });
 
   function getCurrentWordArr() {
     const numberOfGuessedWords = guessedWords.length;
     return guessedWords[numberOfGuessedWords - 1]
   }
 
-  function updateGuessedWords(letter){
+  function updateGuessedLetters(letter){
     const currentWordArr = getCurrentWordArr()
     console.log('current array = ' + currentWordArr)
-    if (getCurrentWordArr && currentWordArr.length < numofLetters){
+    if (currentWordArr && currentWordArr.length < numofLetters){
       currentWordArr.push(letter)
-      const availableSpaceEl = document.getElementById(String(availableSpace))
+      const availableSpaceEl = document.getElementById(availableSpace)
       availableSpace++;
       availableSpaceEl.textContent = letter;
     }
   }
 
-  function getTileColor(letter, index){
-    let letterUpper = letter.toUpperCase();
-    const isCorrectLetter = wordle.includes(letterUpper);
-    console.log("wordle is = " + wordle + " letter = "+ letterUpper)
-    if (!isCorrectLetter){
-      return "rgb(58, 58,60)";
+  function getIndicesOfLetter(letter, arr){
+    const indices = [];
+    let idx = arr.indexOf(letter);
+    while (idx != -1){
+      indices.push(idx);
+      idx = arr.indexOf(letter, idx+1);
+    }
+    return indices;
+  } // END OF getIndicesOfLetter
+
+  function getTileClass(letter, index, currentWordArr) {
+    const isCorrectLetter = wordle
+      .toUpperCase()
+      .includes(letter.toUpperCase());
+      if (!isCorrectLetter){
+        return "incorrect-letter";
+      }
+
+      const letterInThatPosition = wordle.charAt(index);
+      const isCorrectPosition = letter.toLowerCase() === letterInThatPosition.toLowerCase();
+      if (isCorrectPosition){
+        return "correct-letter-in-place";
+      }
+
+  
+       const isGuessedMoreThanOnce =
+       currentWordArr.filter((l) => l === letter).length > 1;
+
+       if (!isGuessedMoreThanOnce) {
+        return "correct-letter";
+      }
+
+      const existsMoreThanOnce = wordle.split("").filter((l) => l === letter).length > 1;
+
+    // is guessed more than once and exists more than once
+    if (existsMoreThanOnce) {
+      return "correct-letter";
     }
 
-    const letterInThatPosition = wordle.charAt(index);
-    const isCorrectPosition = (letterUpper === letterInThatPosition);
-    
-    if (isCorrectPosition){
-      return "rgb(83, 141, 78)";
+    const hasBeenGuessedAlready = currentWordArr.indexOf(letter) < index;
+
+    const indices = getIndicesOfLetter(letter, wordle.split(""));
+    const otherIndices = indices.filter((i) => i !== index);
+    const isGuessedCorrectlyLater = otherIndices.some(
+      (i) => i > index && currentWordArr[i] === letter
+    );
+
+    if (!hasBeenGuessedAlready && !isGuessedCorrectlyLater) {
+      return "correct-letter";
     }
-    
-    return "rgb(181, 159, 59)";
-  }
+
+    return "incorrect-letter";
+
+
+} // END OF getTileClass
+
 
   function handleSubmitWord(){
     const currentWordArr = getCurrentWordArr();
-    if (currentWordArr.length !== numofLetters){
+    const guessedWord = currentWordArr.join("");
+    if (guessedWord.length !== numofLetters){
       messageContainerEl.innerText = (`current length = ${currentWordArr.length} which is less than ${numofLetters} letters.`);
       return;
     }
 
-    const currentWord = currentWordArr.join("");
-    let result = currentWord.toUpperCase();
+    let result = guessedWord.toUpperCase();
 
     const firstLetterId = guessedWordCount * numofLetters + 1;
-    const interval = 500;
+    let interval = 2500 / numofLetters;
+
     currentWordArr.forEach((letter, index) => {
       setTimeout(() => {
-        const tileColor = getTileColor(letter, index);
-
-        const letterId = firstLetterId + index;
-        const letterEl = document.getElementById(letterId);
-        letterEl.classList.add("animate__flipInX")
+        const tileClass = getTileClass(letter, index, currentWordArr);
+        if (tileClass){
+          const letterId = firstLetterId + index;
+          console.log("letter = " + letter);
+          const letterEl = document.getElementById(letterId);
+       //   if (letter === " "){
+       //     letter = "space"
+       //   }
+       //  let keyboardLetter = letter.toLowerCase();
+       //  const keyboardLetterEl = document.querySelector("[data-key=" + keyboardLetter + "]");
+      //   console.log("keyboard letter = " + keyboardLetterEl)
+          letterEl.classList.add("animate__flipInX")
+          console.log ("class should have been set to flip")
+          letterEl.classList.add(tileClass)
       //  letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-        letterEl.style.backgroundColor = tileColor;
-        letterEl.style.borderColor = tileColor;
-      }, interval * index)
-    })
+    //    letterEl.style.backgroundColor = tileColor;
+    //    letterEl.style.borderColor = tileColor;
+    //    keyboardLetterEl.style.backgroundColor = tileColor;
+          const keyboardEl = document.querySelector(`[data-key=${letter}]`);
+          console.log ("keyboardEl = " + keyboardEl);
+          keyboardEl.classList.add(tileClass);
+        }
+      }, interval * index);  // END OF setTimeout
+    });  // END OF forEach letter in the current word
 
     guessedWordCount += 1;
-
-    if (result === wordle){
+    console.log("guessed word = " + guessedWord + "  wordle = " + wordle)
+    let guessedWordUpper = guessedWord.toUpperCase();
+    if (guessedWordUpper === wordle){
       messageContainerEl.innerText = "Congratulations!"
     }
 
-    if (guessedWords.length ===  numofGuesses) {
+    if (guessedWords.length ===  numofGuesses && guessedWord !== wordle) {
       window.alert(`Sorry, no more guesses. The wordle is ${wordle}`)
     } 
 
 
     guessedWords.push([]);
     console.log("Guessedwords length = " + guessedWords.length)
-  }
+  } // END OF handleSubmitWord
 
   function createSquares(){
     let screenWidth = window.innerWidth;
@@ -118,14 +229,15 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const gameBoard = document.getElementById("board")
 
-    for (let index = 0; index < (numofLetters*numofGuesses); index++) {
+    for (let i = 0; i < (numofLetters*numofGuesses); i++) {
       let square = document.createElement("div");
       square.classList.add("square");
       square.classList.add("animate__animated");
-      square.setAttribute("id", index + 1);
+      square.setAttribute("id", i + 1);
       gameBoard.appendChild(square)
     }  
 
+    //  FROM HERE TO END OF FUNCTION ADDED BECAUSE NUMBER OF LETTERS IN WORDLE CAN VARY
     const board = document.getElementById("board")
     const square = document.querySelectorAll(".square")
     console.log(square)
@@ -135,32 +247,69 @@ document.addEventListener("DOMContentLoaded", () => {
       square.style.height = (screenWidth / (numofLetters + 3) + 'px');
     });
     board.style.gridTemplateColumns = 'repeat(' + numofLetters + ', 1fr)'
-  }
+  
+  } // END OF createSquares
 
-  function handleDeleteLetter(){
+  function handleDelete(){
     const currentWordArr = getCurrentWordArr()
     console.log(currentWordArr)
     if (currentWordArr.length === 0){
       console.log("blank array")
       return;
     }
-    const removedLetter = currentWordArr.pop();
+    currentWordArr.pop();
 
     guessedWords[guessedWords.length - 1] = currentWordArr
 
-    const lastLetterEl = document.getElementById(String(availableSpace - 1))
+    const lastLetterEl = document.getElementById(availableSpace - 1)
 
-    lastLetterEl.textContent = "" 
+    lastLetterEl.innerHTML = ""   
     availableSpace--; 
 
-  }
+  } // END OF handleDelete
 
-  for (let i = 0; i < keys.length; i++){
-    keys[i].onclick = ({target}) => {
-      const letter = target.getAttribute("data-key")
-      console.log(letter);
-      messageContainerEl.innerText = ""
 
+  function addKeyboardClicks(){    
+
+      const keys = document.querySelectorAll(".keyboard-row button");
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].addEventListener("click", ({ target }) => {
+          messageContainerEl.innerText = ""
+          let containerEl = document.getElementById('container')
+          containerEl.requestFullscreen();
+          let key = target.getAttribute("data-key");
+  
+ 
+          if (key === "enter") {
+            handleSubmitWord();
+            return;
+          }
+  
+          if (key === "del") {
+            handleDelete();
+            return;
+          }
+
+          if (key === "space") {
+            key = " "
+          }
+  
+          updateGuessedLetters(key);
+        });
+      }
+    } // END OF Ians addKeyboardClicks 
+
+
+
+
+
+
+/*   MAY WANT TO ADD SOME OF THIS INTO ABOVE FUNCTION
+
+      var audio = new Audio("./keypress.wav");
+      audio.play();
+      //let containerEl = document.getElementById('container')
+      //containerEl.requestFullscreen();
       if (letter === "enter" ){
         handleSubmitWord();
         return;
@@ -172,8 +321,46 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
 
+      if (letter === 'space'){
+        letter = ' ';
+      }
+      console.log('clicked letter = ' + letter )
       updateGuessedWords(letter);
+    }  // END OF addKeyboardClicks 
+*/
+
+
+  function handleKeystroke(letter){
+
+    console.log ("key in handleKeystroke function - "  + letter)
+    messageContainerEl.innerText = "";
+    let containerEl = document.getElementById('container')
+  //  containerEl.requestFullscreen();
+
+  if (letter === "Enter" || letter === "Delete"){
+    console.log ("key in handleKeystroke function2 - "  + letter)
+  } else {
+      let alpha = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+      let result = alpha.includes(letter);
+      if (!result){
+        return;
+      }
     }
+    if (letter === "Enter" ){
+      handleSubmitWord();
+      return;
+    }
+
+    if (letter === "Delete" ){
+      console.log("available space = " + availableSpace)
+      handleDelete();
+      return;
+    }
+
+
+
+    updateGuessedLetters(letter);
+
   }
 
 })
